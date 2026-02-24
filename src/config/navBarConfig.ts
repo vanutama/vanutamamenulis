1e1865fdcae40acbef3f79ae1a0e1415fb67862f
vanutama <vanutama@gmail.com>
2026-02-24 21:47:12 +0700

    Set navbar

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -18,71 +18,51 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
     LinkPreset.Archive,
   ];
 
-	// 友链
-	links.push(LinkPreset.Friends);
+  // 关于及其子菜单
+  links.push({
+    name: "About",
+    url: "/content/",
+    icon: "material-symbols:info",
+    children: [
+      // 根据配置决定是否添加赞助，在siteConfig关闭pages.sponsor时导航栏不显示赞助
+      ...(siteConfig.pages.sponsor ? [LinkPreset.Sponsor] : []),
 
       // 关于页面
       LinkPreset.About,
+    ],
+  });
 
-	// 我的及其子菜单
-	links.push({
-		name: "我的",
-		url: "/my/",
-		icon: "material-symbols:person",
-		children: [
-			// 根据配置决定是否添加相册，在siteConfig关闭pages.gallery时导航栏不显示相册
-			...(siteConfig.pages.gallery ? [LinkPreset.Gallery] : []),
+  // 自定义导航栏链接,并且支持多级菜单
+  links.push({
+    name: "Link",
+    url: "/links/",
+    icon: "material-symbols:link",
 
-			// 根据配置决定是否添加番组计划，在siteConfig关闭pages.bangumi时导航栏不显示番组计划
-			...(siteConfig.pages.bangumi ? [LinkPreset.Bangumi] : []),
-		],
-	});
+    // 子菜单
+    children: [
+      {
+        name: "GitHub",
+        url: "https://github.com/CuteLeaf/Firefly",
+        external: true,
+        icon: "fa7-brands:github",
+      },
+      {
+        name: "Gitee",
+        url: "https://gitee.com/CuteLeaf/Firefly",
+        external: true,
+        icon: "fa7-brands:gitee",
+      },
+      {
+        name: "QQ交流群",
+        url: "https://qm.qq.com/q/ZGsFa8qX2G",
+        external: true,
+        icon: "fa7-brands:qq",
+      },
+    ],
+  });
 
-	// 关于及其子菜单
-	links.push({
-		name: "关于",
-		url: "/content/",
-		icon: "material-symbols:info",
-		children: [
-			// 根据配置决定是否添加赞助，在siteConfig关闭pages.sponsor时导航栏不显示赞助
-			...(siteConfig.pages.sponsor ? [LinkPreset.Sponsor] : []),
-
-			// 关于页面
-			LinkPreset.About,
-		],
-	});
-
-	// 自定义导航栏链接,并且支持多级菜单
-	links.push({
-		name: "链接",
-		url: "/links/",
-		icon: "material-symbols:link",
-
-		// 子菜单
-		children: [
-			{
-				name: "GitHub",
-				url: "https://github.com/CuteLeaf/Firefly",
-				external: true,
-				icon: "fa7-brands:github",
-			},
-			{
-				name: "Gitee",
-				url: "https://gitee.com/CuteLeaf/Firefly",
-				external: true,
-				icon: "fa7-brands:gitee",
-			},
-			{
-				name: "QQ交流群",
-				url: "https://qm.qq.com/q/ZGsFa8qX2G",
-				external: true,
-				icon: "fa7-brands:qq",
-			},
-		],
-	});
-
-	// 仅返回链接，其它导航搜索相关配置在模块顶层常量中独立导出
-	return { links } as NavBarConfig;
+  // 仅返回链接，其它导航搜索相关配置在模块顶层常量中独立导出
+  return { links } as NavBarConfig;
 };
 
 // 导航搜索配置
