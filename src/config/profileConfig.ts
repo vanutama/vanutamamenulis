a5b7df2a59cc5f3ccc07ac57c75c4ad6327dc6c7
vanutama <vanutama@gmail.com>
2026-02-25 21:32:53 +0700

    Edit profile

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -10,36 +10,30 @@ export const profileConfig: ProfileConfig = {
   // 个人签名
   bio: "📚 Avid Reader • ✝️ Bible Explorer • 💻 Tech Enthusiast • 🎨 Design Newbie • ⌨️ Code Learner",
 
-	// 链接配置
-	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
-	// 访问https://icones.js.org/ 获取图标代码，
-	// 如果想使用尚未包含相应的图标集，则需要安装它
-	// `pnpm add @iconify-json/<icon-set-name>`
-	// showName: true 时显示图标和名称，false 时只显示图标
-	links: [
-		{
-			name: "qq",
-			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
-			showName: false,
-		},
-		{
-			name: "GitHub",
-			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
-			showName: false,
-		},
-		{
-			name: "Email",
-			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
-			showName: false,
-		},
-		{
-			name: "RSS",
-			icon: "fa7-solid:rss",
-			url: "/rss/",
-			showName: false,
-		},
-	],
+  // 链接配置
+  // 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
+  // 访问https://icones.js.org/ 获取图标代码，
+  // 如果想使用尚未包含相应的图标集，则需要安装它
+  // `pnpm add @iconify-json/<icon-set-name>`
+  // showName: true 时显示图标和名称，false 时只显示图标
+  links: [
+    {
+      name: "Instagram",
+      icon: "fa7-brands:instagram",
+      url: "https://www.instagram.com/vanutamamenulis/",
+      showName: false,
+    },
+    {
+      name: "Email",
+      icon: "fa7-solid:envelope",
+      url: "mailto:vanutamamenulis@gmail.com",
+      showName: false,
+    },
+    {
+      name: "RSS",
+      icon: "fa7-solid:rss",
+      url: "/rss/",
+      showName: false,
+    },
+  ],
 };
