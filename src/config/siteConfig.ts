93b338a6a8480d93f05c6042dba0375b8148572c
vanutama <vanutama@gmail.com>
2026-05-27 21:37:19 +0700

    Change Language

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -3,7 +3,7 @@ import { fontConfig } from "./fontConfig";
 
 // 定义站点语言
 // 语言代码，例如：'zh_CN', 'zh_TW', 'en', 'ja', 'ru'。
-const SITE_LANG = "zh_CN";
+const SITE_LANG = "en";
 
 export const siteConfig: SiteConfig = {
   // 站点标题
@@ -37,7 +37,7 @@ export const siteConfig: SiteConfig = {
     // 是否对访问者隐藏主题色选择器
     fixed: false,
     // 默认模式："light" 亮色，"dark" 暗色，"system" 跟随系统
-    defaultMode: "system",
+    defaultMode: "dark",
   },
 
   // 页面整体宽度（单位：rem）
